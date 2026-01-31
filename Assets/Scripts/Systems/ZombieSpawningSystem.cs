2030d538712c75910019e1f9f082c28cdd9cbb62
Matt Russiello <russiello.matt@gmail.com>
2026-01-31 11:26:09 -0500

    Update ZombieSpawningSystem.cs

--- Assets/Scripts/Systems/ZombieSpawningSystem.cs
+++ Assets/Scripts/Systems/ZombieSpawningSystem.cs
@@ -1,14 +1,44 @@
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.VisualScripting;
+using Random = Unity.Mathematics.Random;
+
 
 partial struct ZombieSpawningSystem : ISystem
 {
+
+    double nextSpawn;
+
+    Random random;
+
     public void OnCreate(ref SystemState state)
     {
-
+        random = new Random((uint)UnityEngine.Random.Range(1, int.MaxValue));
     }
 
     public void OnUpdate(ref SystemState state)
     {
-        
+        double CurrentTime = SystemAPI.Time.ElapsedTime;
+
+
+        if (CurrentTime < nextSpawn)
+        {
+            nextSpawn = CurrentTime + 1;
+        }
+
+        foreach (var (spawner, entity) in SystemAPI.Query<RefRW<ZombieSpawner>>().WithEntityAccess())
+        {
+
+            float3 nextPosition = random.NextFloat3() * 10 + spawner.ValueRO.SpawnZone.Center;
+
+            Entity zombie = state.EntityManager.Instantiate(spawner.ValueRO.Prefab);
+
+            SystemAPI.GetComponentRW<LocalTransform>(zombie).ValueRW.Position = nextPosition;
+
+        }
+
+
+
     }
 }
