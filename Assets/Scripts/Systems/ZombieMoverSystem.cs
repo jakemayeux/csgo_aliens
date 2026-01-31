a6610605b70d9b6c8ff4c21564aef6edc51aec2c
Matt Russiello <russiello.matt@gmail.com>
2026-01-31 12:15:34 -0500

    Update ZombieMoverSystem.cs

--- Assets/Scripts/Systems/ZombieMoverSystem.cs
+++ Assets/Scripts/Systems/ZombieMoverSystem.cs
@@ -1,3 +1,5 @@
+using Unity.Collections;
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -14,19 +16,60 @@ partial struct ZombieMoverSystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-        foreach(var (mover, transform, entity) in SystemAPI.Query<RefRW<ZombieMover>, RefRW<LocalTransform>>().WithEntityAccess())
+        if (false)
         {
-            
-            //Calculate next position 
-            float3 destination = mover.ValueRO.destination;
-            float3 currentPosition = transform.ValueRO.Position;
-            
-            float3 trajectory = math.normalize(destination - currentPosition);
-            
-            float3 nextPosition = currentPosition + trajectory* mover.ValueRO.speed * SystemAPI.Time.DeltaTime;
-            
-            transform.ValueRW.Position = nextPosition;
-            
+            foreach (var (mover, transform, entity) in SystemAPI.Query<RefRW<ZombieMover>, RefRW<LocalTransform>>().WithEntityAccess())
+            {
+
+                //Calculate next position 
+                float3 destination = mover.ValueRO.destination;
+                float3 currentPosition = transform.ValueRO.Position;
+
+                float3 trajectory = math.normalize(destination - currentPosition);
+
+                float3 nextPosition = currentPosition + trajectory * mover.ValueRO.speed * SystemAPI.Time.DeltaTime;
+
+                transform.ValueRW.Position = nextPosition;
+
+            }
         }
+
+        ZombieMoverJob moverJob = new ZombieMoverJob
+        {
+            DeltaTime = SystemAPI.Time.DeltaTime
+        };
+
+        state.Dependency = moverJob.ScheduleParallel(state.Dependency);
     }
+}
+
+public struct DeathComponent : IComponentData
+{
+
+}
+
+
+[BurstCompile]
+[WithAbsent(typeof(DeathComponent))]
+
+partial struct ZombieMoverJob : IJobEntity
+{
+    [ReadOnly] public float DeltaTime;
+
+    public void Execute(Entity entity, in ZombieMover mover, ref LocalTransform transform)
+    {
+
+        float3 destination = mover.destination;
+        float3 currentPosition = transform.Position;
+
+        float3 trajectory = math.normalize(destination - currentPosition);
+
+        float3 nextPosition = currentPosition + trajectory * mover.speed * DeltaTime;
+
+        transform.Position = nextPosition;
+
+    }
+
+
+
 }
